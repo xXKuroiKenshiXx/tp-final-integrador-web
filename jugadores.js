bd5024e5259aa2c3924a39bf9852401052ce9b0f
LucasBec <89425792+LucasBec@users.noreply.github.com>
2023-06-14 23:13:21 -0300

    se siguió pagina jugadores

--- jugadores.js
+++ jugadores.js
@@ -26,28 +26,62 @@ document.addEventListener("DOMContentLoaded", function() {
   
     // Cargar jugadores almacenados en localStorage al cargar la página
     cargarJugadores();
-  
+
     jugadorForm.addEventListener("submit", function(e) {
       e.preventDefault();
       // Obtener los valores ingresados en el formulario
-      const documento = documentoInput.value.trim();
+      const documento = documentoInput.value.trim(); //.trim() es para eliminar los espacios en blanco al inicio y al final de una cadena de texto
       const nombre = nombreInput.value.trim();
       const apellido = apellidoInput.value.trim();
       const posicion = posicionSelect.value;
       const dorsal = dorsalInput.value;
       const pieHabil = pieHabilSelect.value;
+      // Capitalizar la primera letra de cada palabra en el nombre
+      const nombreCapitalizado = nombre
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+
+      // Capitalizar la primera letra de cada palabra en el apellido
+      const apellidoCapitalizado = apellido
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
   
       // Validar que se hayan ingresado todos los campos
       if (documento === "" || nombre === "" || apellido === "" || posicion === "" || dorsal === "" || pieHabil === "") {
         alert("Por favor, completa todos los campos.");
         return;
       }
+      // Validar el número de documento (solo números)
+      if (!/^\d+$/.test(documento)) {
+        alert('El número de documento debe ser numérico');
+        return;
+      }
+
+      // Validar el nombre (no debe contener números)
+      if (/\d/.test(nombre)) {
+        alert('El nombre no debe contener números');
+        return;
+      }
+
+      // Validar el apellido (no debe contener números)
+      if (/\d/.test(apellido)) {
+        alert('El apellido no debe contener números');
+        return;
+      }
+
+      // Validar el dorsal (no debe ser mayor a 99)
+      if (parseInt(dorsal) > 99) {
+        alert('El dorsal no puede ser mayor a 99');
+        return;
+      }
   
       // Crear un objeto jugador con los datos ingresados
       const jugador = {
         documento,
-        nombre,
-        apellido,
+        nombreCapitalizado,
+        apellidoCapitalizado,
         posicion,
         dorsal,
         pieHabil
@@ -91,8 +125,8 @@ document.addEventListener("DOMContentLoaded", function() {
         const fila = document.createElement("tr");
         fila.innerHTML = `
           <td>${jugador.documento}</td>
-          <td>${jugador.nombre}</td>
-          <td>${jugador.apellido}</td>
+          <td>${jugador.nombreCapitalizado}</td>
+          <td>${jugador.apellidoCapitalizado}</td>
           <td>${jugador.posicion}</td>
           <td>${jugador.dorsal}</td>
           <td>${jugador.pieHabil}</td>
@@ -108,6 +142,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
       });
+      
 
       // Agregar event listeners a los botones de editar y eliminar
       const editarButtons = document.getElementsByClassName("editar-btn");
@@ -136,8 +171,8 @@ document.addEventListener("DOMContentLoaded", function() {
   
       // Llenar el formulario con los datos del jugador
       documentoInput.value = jugador.documento;
-      nombreInput.value = jugador.nombre;
-      apellidoInput.value = jugador.apellido;
+      nombreInput.value = jugador.nombreCapitalizado;
+      apellidoInput.value = jugador.apellidoCapitalizado;
       posicionSelect.value = jugador.posicion;
       dorsalInput.value = jugador.dorsal;
       pieHabilSelect.value = jugador.pieHabil;
@@ -167,5 +202,19 @@ document.addEventListener("DOMContentLoaded", function() {
       // Actualizar la tabla de jugadores
       cargarJugadores();
     }
+
+
+    //funcion para buscar jugadores
+    function buscarJugador(){
+
+    }
   });
-  
+
+
+
+
+
+
+
+
+
